5785e0c79bfbc9afa6b17b07571cb9bfdffec82f
Nacho286 <igna_nacho286@hotmail.com>
2017-05-29 20:44:41 -0300

    cambio

--- main.pl
+++ main.pl
@@ -3,4 +3,4 @@ herramienta(volatilizador,40).
 herramienta(encendedor,5).
 
 composicion(binaria(X,Y),P,5):- herramienta(X, PX), herramienta(Y,PY), P is 2*PX + PY.
-composicion(jerarquica(X,Y),P,C) :- X = herramienta(A,PA), Y = herramienta(B,PB), C is 2, P is PA*PB
+composicion(jerarquica(X,Y),P,C) :- herramienta(X,PA), herramienta(Y,PB), C is 2, P is PA*PB.
