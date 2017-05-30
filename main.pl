d9f9d67424c9a75974bca8315c97fbda63fe0b24
klascolin <klascolin@gmail.com>
2017-05-29 22:50:13 -0300

    Esta version tiene mas sentido. Estaba mal pensada la recursion, creo

--- main.pl
+++ main.pl
@@ -11,4 +11,6 @@ composicion(binaria(X,Y),P,5):- herramienta(X, PX), herramienta(Y,PY), P is 2*PX
 %Caso base
 composicion(jerarquica(X,Y),P,C) :- herramienta(X,PX), herramienta(Y,PY), C is 2, P is PX*PY.
 %Caso general
-composicion(jerarquica(binaria(X,Y),Z),P,C) :- herramienta(Z,PZ), composicion(binaria(X,Y),PB,CB), P is PB*PZ, C is CB+1.
+composicion(jerarquica(X,Y),P,C) :- herramienta(Y,PY), composicion(X,PX,CX),  P is PX*PY, C is CX+1.
+composicion(jerarquica(X,Y),P,C) :- herramienta(X,PX), composicion(Y,PY,CY),  P is PX*PY, C is CY+1.  
+composicion(jerarquica(X,Y),P,C) :- composicion(X,PX,CX), composicion(Y,PY,CY),  P is PX*PY, C is CX+CY.
