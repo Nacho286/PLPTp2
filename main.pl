3d29ba522ec2100bcbb26cfe3073b381b9dde97b
klascolin <klascolin@gmail.com>
2017-05-29 22:22:03 -0300

    Agrego otros casos de composicion jerarquica

--- main.pl
+++ main.pl
@@ -3,4 +3,12 @@ herramienta(volatilizador,40).
 herramienta(encendedor,5).
 
 composicion(binaria(X,Y),P,5):- herramienta(X, PX), herramienta(Y,PY), P is 2*PX + PY.
-composicion(jerarquica(X,Y),P,C) :- herramienta(X,PA), herramienta(Y,PB), C is 2, P is PA*PB.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Composicion de jerarquicas
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Caso base
+composicion(jerarquica(X,Y),P,C) :- herramienta(X,PX), herramienta(Y,PY), C is 2, P is PX*PY.
+%Caso general
+composicion(jerarquica(binaria(X,Y),Z),P,C) :- herramienta(Z,PZ), composicion(binaria(X,Y),PB,CB), P is PB*PZ, C is CB+1.
