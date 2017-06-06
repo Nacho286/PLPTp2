ffd6e25459c7c33a3f87beb2a8613b8dd37875d1
klascolin <klascolin@gmail.com>
2017-06-06 02:16:56 -0300

    Ahora no explota el 4

--- main.pl
+++ main.pl
@@ -49,4 +49,4 @@ masPoderosa(M1,M2):- configuracion(M1,_,P,_),not((configuracion(M2,_,P2,_),P<P2)
 %Mejor
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %mejor(+M1,+M2)
-%mejor(M1,M2):- not(configuracion(M2,Conf,P,C), not(configuracion(M1,Conf,PP,CC), (PP > P, C > CC)) ).
+mejor(M1,M2):- not( (configuracion(M2,Conf,P,C), (not((configuracion(M1,Conf,PP,CC), (PP >= P, C > CC))))) ).
